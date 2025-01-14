ac9015a7f11f23a7342c6997dfc03ce1e7d6c139
Leonardo <leonardogarcarr23@gmail.com>
2025-01-13 20:43:36 -0500

    register done

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,18 +1,28 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from './entities/user.entity';
 import { User } from './entities/user.entity';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+
     MongooseModule.forFeature([
       {
         name: User.name,
         schema: UserSchema
       }
     ]),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SEED,
+      signOptions: { expiresIn: '6h' },
+    }),
   ],
   controllers: [AuthController],
   providers: [AuthService],
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,17 +4,20 @@ import { Model } from 'mongoose';
 
 import * as bcryptjs from 'bcryptjs';
 
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
-import { LoginDto } from './dto/login.dto';
+import { LoginDto, RegisterUserDto, CreateUserDto, UpdateAuthDto } from './dto';
 import { User } from './entities/user.entity';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './interfaces/jwt.payload';
+import { LoginResponse } from './interfaces/login-response';
 
 @Injectable()
 export class AuthService {
 
   constructor(
     @InjectModel(User.name)
-    private userModel: Model<User>
+    private userModel: Model<User>,
+
+    private jwtService: JwtService,
   ) { }
 
   async create(createUserDto: CreateUserDto): Promise<User> {
@@ -48,7 +51,19 @@ export class AuthService {
 
   }
 
-  async login(loginDto: LoginDto) {
+  async register(registerDto: RegisterUserDto): Promise<LoginResponse> {
+
+    const user = await this.create(registerDto);
+    console.log(user);
+
+    return {
+      user,
+      token: this.getJwtToken({ id: user._id })
+    }
+
+  }
+
+  async login(loginDto: LoginDto): Promise<LoginResponse> {
 
     const { email, password } = loginDto;
 
@@ -66,7 +81,7 @@ export class AuthService {
 
     return {
       user: rest,
-      token: 'ABC-123'
+      token: this.getJwtToken({ id: user.id })
     };
 
   }
@@ -86,4 +101,10 @@ export class AuthService {
   remove(id: number) {
     return `This action removes a #${id} auth`;
   }
+
+  getJwtToken(payload: JwtPayload) {
+    const token = this.jwtService.sign(payload);
+    return token;
+  }
+
 }
